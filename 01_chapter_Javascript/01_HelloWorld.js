28982bd06ba7fbb7f1931c6f41697c40dd0fdcf6
HarshalDahake <harshaldahake@gmail.com>
2026-07-13 13:35:54 +0530

    Update HelloWorld.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- 01_chapter_Javascript/01_HelloWorld.js
+++ 01_chapter_Javascript/01_HelloWorld.js
@@ -1,7 +1,1 @@
 console.log("Hello 'Harshal Dahake Welcome to JavaScript Programming!");
-
-console.log("Hello 'Harshal Dahake Welcome to JavaScript Programming2!");
-
-console.log("Hello 'Harshal Dahake Welcome to JavaScript Programming!");
-
-console.log("Hello 'Harshal Dahake Welcome to JavaScript Programming2!");
